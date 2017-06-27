ddb9bf7211a7639cdffa3fb653829ef66bf742a9
Carl Pearson <pearson@illinois.edu>
2017-06-27 08:24:59 +0200

    add automatic formatting for vscode

--- callbacks.cpp
+++ callbacks.cpp
@@ -1,10 +1,10 @@
-#include <vector>
+#include <array>
 #include <cassert>
+#include <map>
 #include <memory>
 #include <stdexcept>
 #include <string>
-#include <array>
-#include <map>
+#include <vector>
 
 #include <cstdio>
 #include <cstdlib>
@@ -16,24 +16,21 @@
 #include "allocation.hpp"
 #include "value.hpp"
 
-#define CHECK_CU_ERROR(err, cufunc)                                     \
-  if (err != CUDA_SUCCESS)                                              \
-    {                                                                   \
-      printf ("%s:%d: error %d for CUDA Driver API function '%s'\n",    \
-              __FILE__, __LINE__, err, cufunc);                         \
-      exit(-1);                                                         \
-    }
+#define CHECK_CU_ERROR(err, cufunc)                                            \
+  if (err != CUDA_SUCCESS) {                                                   \
+    printf("%s:%d: error %d for CUDA Driver API function '%s'\n", __FILE__,    \
+           __LINE__, err, cufunc);                                             \
+    exit(-1);                                                                  \
+  }
 
-#define CHECK_CUPTI_ERROR(err, cuptifunc)                               \
-  if (err != CUPTI_SUCCESS)                                             \
-    {                                                                   \
-      const char *errstr;                                               \
-      cuptiGetResultString(err, &errstr);                               \
-      printf ("%s:%d:Error %s for CUPTI API function '%s'.\n",          \
-              __FILE__, __LINE__, errstr, cuptifunc);                   \
-      exit(-1);                                                         \
-    }
-
+#define CHECK_CUPTI_ERROR(err, cuptifunc)                                      \
+  if (err != CUPTI_SUCCESS) {                                                  \
+    const char *errstr;                                                        \
+    cuptiGetResultString(err, &errstr);                                        \
+    printf("%s:%d:Error %s for CUPTI API function '%s'.\n", __FILE__,          \
+           __LINE__, errstr, cuptifunc);                                       \
+    exit(-1);                                                                  \
+  }
 
 typedef uint64_t Time;
 
@@ -44,11 +41,12 @@ Time getTimestamp(const CUpti_CallbackData *cbInfo) {
   return Time(time);
 }
 
-void handleMemcpy(Allocations &allocations, Values &values, const CUpti_CallbackData *cbInfo) {
+void handleMemcpy(Allocations &allocations, Values &values,
+                  const CUpti_CallbackData *cbInfo) {
   if (cbInfo->callbackSite == CUPTI_API_ENTER) {
     auto params = ((cudaMemcpy_v3020_params *)(cbInfo->functionParams));
-    const uintptr_t dst = (uintptr_t) params->dst;
-    const uintptr_t src = (uintptr_t) params->src;
+    const uintptr_t dst = (uintptr_t)params->dst;
+    const uintptr_t src = (uintptr_t)params->src;
     const cudaMemcpyKind kind = params->kind;
     const size_t count = params->count;
 
@@ -97,25 +95,24 @@ void handleMemcpy(Allocations &allocations, Values &values, const CUpti_Callback
   }
 }
 
-
-void handleMalloc(Allocations &allocations, Values &values, const CUpti_CallbackData *cbInfo) {
+void handleMalloc(Allocations &allocations, Values &values,
+                  const CUpti_CallbackData *cbInfo) {
   if (cbInfo->callbackSite == CUPTI_API_ENTER) {
   } else if (cbInfo->callbackSite == CUPTI_API_EXIT) {
-  auto params = ((cudaMalloc_v3020_params *)(cbInfo->functionParams));
-  uintptr_t devPtr = (uintptr_t)(*(params->devPtr));
-  const size_t size = params->size;
-  printf("[cudaMalloc] %lu[%lu]\n", devPtr, size);
+    auto params = ((cudaMalloc_v3020_params *)(cbInfo->functionParams));
+    uintptr_t devPtr = (uintptr_t)(*(params->devPtr));
+    const size_t size = params->size;
+    printf("[cudaMalloc] %lu[%lu]\n", devPtr, size);
 
-  std::shared_ptr<Allocation> a(new Allocation(devPtr, size, Allocation::Location::Device));
-  allocations.insert(a);
+    std::shared_ptr<Allocation> a(
+        new Allocation(devPtr, size, Allocation::Location::Device));
+    allocations.insert(a);
 
-  values.insert(std::shared_ptr<Value>(new Value(devPtr, size)));
+    values.insert(std::shared_ptr<Value>(new Value(devPtr, size)));
   } else {
     assert(0 && "How did we get here?");
   }
 }
-
-
 
 typedef struct {
   dim3 gridDim;
@@ -150,14 +147,16 @@ void handleCudaConfigureCall(const CUpti_CallbackData *cbInfo) {
   }
 }
 
-
 void handleCudaSetupArgument(const CUpti_CallbackData *cbInfo) {
   if (cbInfo->callbackSite == CUPTI_API_ENTER) {
     printf("callback: cudaSetupArgument entry\n");
-    const auto params = ((cudaSetupArgument_v3020_params *)(cbInfo->functionParams));
-    const uintptr_t arg = (uintptr_t) *static_cast<const void * const*>(params->arg); // arg is a pointer to the arg.
-    //const size_t size     = params->size;
-    //const size_t offset   = params->offset;
+    const auto params =
+        ((cudaSetupArgument_v3020_params *)(cbInfo->functionParams));
+    const uintptr_t arg =
+        (uintptr_t) * static_cast<const void *const *>(
+                          params->arg); // arg is a pointer to the arg.
+    // const size_t size     = params->size;
+    // const size_t offset   = params->offset;
 
     ConfiguredCall().args.push_back(arg);
   } else if (cbInfo->callbackSite == CUPTI_API_EXIT) {
@@ -166,29 +165,32 @@ void handleCudaSetupArgument(const CUpti_CallbackData *cbInfo) {
   }
 }
 
-
 void handleCudaLaunch(Values &values, const CUpti_CallbackData *cbInfo) {
   if (cbInfo->callbackSite == CUPTI_API_ENTER) {
     printf("callback: cudaLaunch entry\n");
-    //const auto params = ((cudaLaunch_v3020_params *)(cbInfo->functionParams));
-    //const uintptr_t func = (uintptr_t) params->func;
+    // const auto params = ((cudaLaunch_v3020_params
+    // *)(cbInfo->functionParams));
+    // const uintptr_t func = (uintptr_t) params->func;
 
     // Find all values that are used by arguments
     std::vector<size_t> argValIds;
-    for (size_t argIdx = 0; argIdx < ConfiguredCall().args.size(); ++argIdx) { // for each kernel argument
-      //printf("arg %lu, val %lu\n", argIdx, valIdx);
+    for (size_t argIdx = 0; argIdx < ConfiguredCall().args.size();
+         ++argIdx) { // for each kernel argument
+      // printf("arg %lu, val %lu\n", argIdx, valIdx);
       bool found;
       uintptr_t valIdx;
-      std::tie(found, valIdx) = values.get_last_overlapping_value(ConfiguredCall().args[argIdx], 1);
+      std::tie(found, valIdx) =
+          values.get_last_overlapping_value(ConfiguredCall().args[argIdx], 1);
       if (found) {
         argValIds.push_back(valIdx);
       }
     }
 
-    // create a new value for each argument. All of these depend on all the argument values
+    // create a new value for each argument. All of these depend on all the
+    // argument values
     for (size_t i = 0; i < ConfiguredCall().args.size(); ++i) {
       const auto &arg = ConfiguredCall().args[i];
-      std::shared_ptr<Value> newValue( new Value(arg, 0));
+      std::shared_ptr<Value> newValue(new Value(arg, 0));
       for (const auto &id : argValIds) {
         newValue->depends_on(id);
       }
@@ -201,76 +203,73 @@ void handleCudaLaunch(Values &values, const CUpti_CallbackData *cbInfo) {
   }
 }
 
-
-std::string getCallbackName(CUpti_CallbackDomain domain, CUpti_CallbackId cbid) {
+std::string getCallbackName(CUpti_CallbackDomain domain,
+                            CUpti_CallbackId cbid) {
   switch (domain) {
-    case CUPTI_CB_DOMAIN_RUNTIME_API:
-      switch (cbid) {
-        case CUPTI_RUNTIME_TRACE_CBID_cudaStreamDestroy_v3020:
-          return std::string("cudaStreamDestroy_v3020");
-        default: return std::string("<unknown runtime api: ") + std::to_string(cbid) + std::string(">");
-      }
-      break;
-    case CUPTI_CB_DOMAIN_DRIVER_API:
-    {
-      switch(cbid) {
-        default: return std::string("<unknown driver api: ") + std::to_string(cbid) + std::string(">");
-      }
-    }
+  case CUPTI_CB_DOMAIN_RUNTIME_API:
+    switch (cbid) {
+    case CUPTI_RUNTIME_TRACE_CBID_cudaStreamDestroy_v3020:
+      return std::string("cudaStreamDestroy_v3020");
     default:
-      return std::string("<unknown domain>");
+      return std::string("<unknown runtime api: ") + std::to_string(cbid) +
+             std::string(">");
+    }
+    break;
+  case CUPTI_CB_DOMAIN_DRIVER_API: {
+    switch (cbid) {
+    default:
+      return std::string("<unknown driver api: ") + std::to_string(cbid) +
+             std::string(">");
+    }
+  }
+  default:
+    return std::string("<unknown domain>");
   }
 }
 
+void CUPTIAPI callback(void *userdata, CUpti_CallbackDomain domain,
+                       CUpti_CallbackId cbid,
+                       const CUpti_CallbackData *cbInfo) {
+  // uint64_t startTimestamp;
+  // uint64_t endTimestamp;
+  (void)userdata;
 
-void CUPTIAPI
-callback(void *userdata, CUpti_CallbackDomain domain,
-         CUpti_CallbackId cbid, const CUpti_CallbackData *cbInfo) {
-  //uint64_t startTimestamp;
-  //uint64_t endTimestamp;
-  (void) userdata;
-      
   // Data is collected for the following APIs
   switch (domain) {
-    case CUPTI_CB_DOMAIN_RUNTIME_API:
-      {
-        switch (cbid) {
-          case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpy_v3020:
-            handleMemcpy(Allocations::instance(), Values::instance(),  cbInfo);
-            break;
-          case CUPTI_RUNTIME_TRACE_CBID_cudaMalloc_v3020:
-            handleMalloc(Allocations::instance(), Values::instance(), cbInfo);
-            break;
-          case CUPTI_RUNTIME_TRACE_CBID_cudaConfigureCall_v3020:
-            handleCudaConfigureCall(cbInfo);
-            break;
-          case CUPTI_RUNTIME_TRACE_CBID_cudaSetupArgument_v3020:
-            handleCudaSetupArgument(cbInfo);
-            break;
-          case CUPTI_RUNTIME_TRACE_CBID_cudaLaunch_v3020:
-            handleCudaLaunch(Values::instance(), cbInfo);
-            break;
-          default:
-            auto name = getCallbackName(domain, cbid);
-            printf("skipping runtime call %s...\n", name.c_str());
-            break;
-        }
-      }
+  case CUPTI_CB_DOMAIN_RUNTIME_API: {
+    switch (cbid) {
+    case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpy_v3020:
+      handleMemcpy(Allocations::instance(), Values::instance(), cbInfo);
       break;
-    case CUPTI_CB_DOMAIN_DRIVER_API:
-      {
-        auto name = getCallbackName(domain, cbid);
-        printf("skipping driver call %s...\n", name.c_str());
-        break;
-      }
+    case CUPTI_RUNTIME_TRACE_CBID_cudaMalloc_v3020:
+      handleMalloc(Allocations::instance(), Values::instance(), cbInfo);
+      break;
+    case CUPTI_RUNTIME_TRACE_CBID_cudaConfigureCall_v3020:
+      handleCudaConfigureCall(cbInfo);
+      break;
+    case CUPTI_RUNTIME_TRACE_CBID_cudaSetupArgument_v3020:
+      handleCudaSetupArgument(cbInfo);
+      break;
+    case CUPTI_RUNTIME_TRACE_CBID_cudaLaunch_v3020:
+      handleCudaLaunch(Values::instance(), cbInfo);
+      break;
     default:
+      auto name = getCallbackName(domain, cbid);
+      printf("skipping runtime call %s...\n", name.c_str());
       break;
+    }
+  } break;
+  case CUPTI_CB_DOMAIN_DRIVER_API: {
+    auto name = getCallbackName(domain, cbid);
+    printf("skipping driver call %s...\n", name.c_str());
+    break;
+  }
+  default:
+    break;
   }
 }
 
-static const char *
-memcpyKindStr(enum cudaMemcpyKind kind)
-{
+static const char *memcpyKindStr(enum cudaMemcpyKind kind) {
   switch (kind) {
   case cudaMemcpyHostToDevice:
     return "HostToDevice";
@@ -283,16 +282,17 @@ memcpyKindStr(enum cudaMemcpyKind kind)
   return "<unknown>";
 }
 
-std::string myexec(const char* cmd) {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) throw std::runtime_error("popen() failed!");
-    while (!feof(pipe.get())) {
-        if (fgets(buffer.data(), 128, pipe.get()) != NULL)
-            result += buffer.data();
-    }
-    return result;
+std::string myexec(const char *cmd) {
+  std::array<char, 128> buffer;
+  std::string result;
+  std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+  if (!pipe)
+    throw std::runtime_error("popen() failed!");
+  while (!feof(pipe.get())) {
+    if (fgets(buffer.data(), 128, pipe.get()) != NULL)
+      result += buffer.data();
+  }
+  return result;
 }
 
 /*
@@ -306,7 +306,7 @@ int main(int argc, char **argv) {
   CUresult cuerr;
   CUptiResult cuptierr;
 
-    
+
   cuerr = cuInit(0);
   CHECK_CU_ERROR(cuerr, "cuInit");
 
@@ -314,11 +314,14 @@ int main(int argc, char **argv) {
   CHECK_CU_ERROR(cuerr, "cuCtxCreate");
 
   CUpti_SubscriberHandle runtimeSubscriber;
-  cuptierr = cuptiSubscribe(&runtimeSubscriber, (CUpti_CallbackFunc)runtimeCallback , &records);
+  cuptierr = cuptiSubscribe(&runtimeSubscriber,
+(CUpti_CallbackFunc)runtimeCallback , &records);
   CHECK_CUPTI_ERROR(cuptierr, "cuptiSubscribe");
-  cuptierr = cuptiEnableDomain(1, runtimeSubscriber, CUPTI_CB_DOMAIN_RUNTIME_API);
+  cuptierr = cuptiEnableDomain(1, runtimeSubscriber,
+CUPTI_CB_DOMAIN_RUNTIME_API);
   CHECK_CUPTI_ERROR(cuptierr, "cuptiEnableDomain");
-  cuptierr = cuptiEnableDomain(1, runtimeSubscriber, CUPTI_CB_DOMAIN_DRIVER_API);
+  cuptierr = cuptiEnableDomain(1, runtimeSubscriber,
+CUPTI_CB_DOMAIN_DRIVER_API);
   CHECK_CUPTI_ERROR(cuptierr, "cuptiEnableDomain");
 
   cudaSetDevice(0);
@@ -341,27 +344,28 @@ int main(int argc, char **argv) {
 }
 */
 
-
 int initCallbacks() {
 
-  //CUdevice device = 0;
-  //CUresult cuerr;
+  // CUdevice device = 0;
+  // CUresult cuerr;
   CUptiResult cuptierr;
 
   CUpti_SubscriberHandle runtimeSubscriber;
-  cuptierr = cuptiSubscribe(&runtimeSubscriber, (CUpti_CallbackFunc)callback , nullptr);
+  cuptierr =
+      cuptiSubscribe(&runtimeSubscriber, (CUpti_CallbackFunc)callback, nullptr);
   CHECK_CUPTI_ERROR(cuptierr, "cuptiSubscribe");
-  cuptierr = cuptiEnableDomain(1, runtimeSubscriber, CUPTI_CB_DOMAIN_RUNTIME_API);
+  cuptierr =
+      cuptiEnableDomain(1, runtimeSubscriber, CUPTI_CB_DOMAIN_RUNTIME_API);
   CHECK_CUPTI_ERROR(cuptierr, "cuptiEnableDomain");
-  cuptierr = cuptiEnableDomain(1, runtimeSubscriber, CUPTI_CB_DOMAIN_DRIVER_API);
+  cuptierr =
+      cuptiEnableDomain(1, runtimeSubscriber, CUPTI_CB_DOMAIN_DRIVER_API);
   CHECK_CUPTI_ERROR(cuptierr, "cuptiEnableDomain");
 
-  //cuptierr = cuptiUnsubscribe(runtimeSubscriber);
-  //CHECK_CUPTI_ERROR(cuptierr, "cuptiUnsubscribe");
+  // cuptierr = cuptiUnsubscribe(runtimeSubscriber);
+  // CHECK_CUPTI_ERROR(cuptierr, "cuptiUnsubscribe");
 
   return 0;
 }
-
 
 void lazyInitCallbacks() {
   static int initialized = 0;
@@ -373,4 +377,3 @@ void lazyInitCallbacks() {
   }
   return;
 }
-
