4ccc87879977380bb7b1e0392f6aa44ca46287e9
Carl Pearson <pearson@illinois.edu>
2017-07-07 12:52:04 -0400

    LD_PRELOAD intercept cublasDgemv

--- prof.cpp
+++ prof.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <cstdio>
+#include <cublas_v2.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <dlfcn.h>
@@ -36,6 +37,26 @@ extern "C" cudaError_t cudaMallocManaged(void **devPtr, size_t size,
   return real_cudaMallocManaged(devPtr, size, flags);
 }
 
+typedef cublasStatus_t (*cublasDgemvFunc)(cublasHandle_t, cublasOperation_t,
+                                          int, int, const double *,
+                                          const double *, int, const double *,
+                                          int, const double *, double *, int);
+static cublasDgemvFunc real_cublasDgemv = nullptr;
+
+extern "C" cublasStatus_t cublasDgemv(cublasHandle_t handle,
+                                      cublasOperation_t trans, int m, int n,
+                                      const double *alpha, const double *A,
+                                      int lda, const double *x, int incx,
+                                      const double *beta, double *y, int incy) {
+  printf("prof.so intercepted cublasDgemv call\n");
+
+  if (real_cublasDgemv == nullptr) {
+    real_cublasDgemv = (cublasDgemvFunc)dlsym(RTLD_NEXT, "cublasDgemv_v2");
+  }
+  assert(real_cublasDgemv && "Will the real cublasDgemv please stand up?");
+  return real_cublasDgemv(handle, trans, m, n, alpha, A, lda, x, incx, beta, y,
+                          incy);
+}
 // typedef cudaError_t
 // (*cudaConfigureCall_t)(dim3,dim3,size_t,cudaStream_t);
 // static cudaConfigureCall_t realCudaConfigureCall = NULL;
