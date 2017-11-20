f8696b87cb0ce4cfd3434b657cacd37417078c15
Carl Pearson <pearson@illinois.edu>
2017-11-20 15:23:18 -0600

    update scripts/cprof2allocinfo.py with new output format

--- scripts/cprof2allocinfo.py
+++ scripts/cprof2allocinfo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 import sys
 import json
@@ -14,68 +14,80 @@ D2A = {}
 A2A = {}
 Values = {}
 
+allocRefs = {}
+
+
 def api_handler(api):
     if type(api) != pycprof.API:
         return
 
     name = api.functionName
     if name == "cudaLaunch" or "cublas" in name or "cudnn" in name:
-        dev = api.device
-        for v_id in api.inputs:
-            v = Values[v_id]
-            a_id = v.allocation_id
-            a = Allocations[a_id]
-            if v.size == 0:
-                v.size = a.size
-            if dev not in A2D:
-                A2D[dev] = {}
-            if a_id not in A2D[dev]:
-                A2D[dev][a_id] = []
-            A2D[dev][a_id] += [v.size]
+        # print name
+        for vId in set(api.inputs + api.outputs):
+            v = Values[vId]
+            a = Allocations[v.allocation_id]
+            if a.loc.type != "host":
+                allocRefs.setdefault(a, [])
+                allocRefs[a] += [api]
 
+    # name = api.functionName
+    # if name == "cudaLaunch" or "cublas" in name or "cudnn" in name:
+    #     dev = api.device
+    #     for v_id in api.inputs:
+    #         v = Values[v_id]
+    #         a_id = v.allocation_id
+    #         a = Allocations[a_id]
+    #         if v.size == 0:
+    #             v.size = a.size
+    #         if dev not in A2D:
+    #             A2D[dev] = {}
+    #         if a_id not in A2D[dev]:
+    #             A2D[dev][a_id] = []
+    #         A2D[dev][a_id] += [v.size]
 
-        for v_id in api.outputs:
-            v = Values[v_id]
-            a_id = v.allocation_id
-            a = Allocations[a_id]
-            if v.size == 0:
-                v.size = a.size
-            if dev not in D2A:
-                D2A[dev] = {}
-            if a_id not in D2A[dev]:
-                D2A[dev][a_id] = []
-            D2A[dev][a_id] += [v.size]
+    #     for v_id in api.outputs:
+    #         v = Values[v_id]
+    #         a_id = v.allocation_id
+    #         a = Allocations[a_id]
+    #         if v.size == 0:
+    #             v.size = a.size
+    #         if dev not in D2A:
+    #             D2A[dev] = {}
+    #         if a_id not in D2A[dev]:
+    #             D2A[dev][a_id] = []
+    #         D2A[dev][a_id] += [v.size]
 
-    else:
-        for vin_id in api.inputs:
-            vin = Values[vin_id]
-            ain_id = vin.allocation_id
-            if ain_id not in A2A:
-                A2A[ain_id] = {}
-            for vout_id in api.outputs:
-                vout = Values[vout_id]
-                aout_id = vout.allocation_id
-                if aout_id not in A2A[ain_id]:
-                    A2A[ain_id][aout_id] = []
-                assert(vin.size == vout.size)
-                # print "adding A2A"
-                A2A[ain_id][aout_id] += [vin.size]
+    # else:
+    #     for vin_id in api.inputs:
+    #         vin = Values[vin_id]
+    #         ain_id = vin.allocation_id
+    #         if ain_id not in A2A:
+    #             A2A[ain_id] = {}
+    #         for vout_id in api.outputs:
+    #             vout = Values[vout_id]
+    #             aout_id = vout.allocation_id
+    #             if aout_id not in A2A[ain_id]:
+    #                 A2A[ain_id][aout_id] = []
+    #             assert(vin.size == vout.size)
+    #             # print "adding A2A"
+    #             A2A[ain_id][aout_id] += [vin.size]
 
-    
 
 def allocation_handler(a):
     if type(a) != pycprof.Allocation:
         return
     Allocations[a.id_] = a
 
-    loc = a.mem.location
-    Id = a.mem.id_
-    if loc not in Memories:
-        Memories[loc] = {}
-    if Id not in Memories[loc]:
-        Memories[loc][Id] = {}
-    
-    Memories[loc][Id][a.id_] = a
+    # loc = a.loc
+    # Id = a.mem.id_
+    # if loc not in Locations:
+    #     Loc[loc] = {}
+    # if Id not in Memories[loc]:
+    #     Memories[loc][Id] = {}
+
+    # Memories[loc][Id][a.id_] = a
+
 
 def value_handler(v):
     if type(v) != pycprof.Value:
@@ -84,39 +96,67 @@ def value_handler(v):
         print "duplicate value", v.id_, "overwriting..."
     Values[v.id_] = v
 
-pycprof.run_handler(allocation_handler)
+
+pycprof.run_handler(allocation_handler, path=sys.argv[1])
 print len(Allocations), "allocations found"
 
-pycprof.run_handler(value_handler)
+pycprof.run_handler(value_handler, path=sys.argv[1])
 print len(Values), "values found"
 
-pycprof.run_handler(api_handler)
+pycprof.run_handler(api_handler, path=sys.argv[1])
 print max(len(D2A), len(A2D)), "devices"
 
-pycprof.set_edge_fields(["Weight"])
-pycprof.set_node_fields(["pos", "size"])
+# pycprof.set_edge_fields(["Weight"])
+# pycprof.set_node_fields(["pos", "size"])
 
 # Add allocation nodes
-for aid, alloc in Allocations.iteritems():
-    node = {"pos": alloc.pos, "size": alloc.size}
-    pycprof.add_node(aid, node)
+# for aid, alloc in Allocations.iteritems():
+#     node = {"pos": alloc.pos, "size": alloc.size}
+#     pycprof.add_node(aid, node)
 
 # add device nodes
-for dev in D2A:
-    pycprof.add_node(dev, {})
-for dev in A2D:
-    pycprof.add_node(dev, {})
+# for dev in D2A:
+#     pycprof.add_node(dev, {})
+# for dev in A2D:
+#     pycprof.add_node(dev, {})
 
 # add edges
-for dev, xfers in D2A.iteritems():
-    for aid, sizes in xfers.iteritems():
-        pycprof.add_edge(dev, aid, {"Weight": math.log(sum(sizes),2)})
-for dev, xfers in A2D.iteritems():
-    for aid, sizes in xfers.iteritems():
-        pycprof.add_edge(aid, dev, {"Weight": math.log(sum(sizes),2)})
-for src, dsts in A2A.iteritems():
-    for dst in dsts:
-        pycprof.add_edge(src, dst, {"Weight": math.log(sum(sizes),2)})
+# for dev, xfers in D2A.iteritems():
+#     for aid, sizes in xfers.iteritems():
+#         pycprof.add_edge(dev, aid, {"Weight": math.log(sum(sizes), 2)})
+# for dev, xfers in A2D.iteritems():
+#     for aid, sizes in xfers.iteritems():
+#         pycprof.add_edge(aid, dev, {"Weight": math.log(sum(sizes), 2)})
+# for src, dsts in A2A.iteritems():
+#     for dst in dsts:
+#         pycprof.add_edge(src, dst, {"Weight": math.log(sum(sizes), 2)})
 
-pycprof.write_nodes("nodes")
-pycprof.write_edges("edges")
+# pycprof.write_nodes("nodes")
+# pycprof.write_edges("edges")
+
+
+def bin(i):
+    return int(math.log(i, 2))
+
+
+maxBin = max(bin(a.size) for a in Allocations.itervalues())
+countHistogram = [0 for i in range(maxBin + 1)]
+sizeHistogram = [0 for i in range(maxBin + 1)]
+
+
+for i, a in Allocations.iteritems():
+    if a.loc.type != "host":
+        countHistogram[bin(a.size)] += 1
+        sizeHistogram[bin(a.size)] += a.size
+        if a in allocRefs:
+            print a.loc.type, a.size, [api.functionName for api in allocRefs[a]]
+
+print "Size Histogram"
+print sizeHistogram
+
+print "Count Histogram"
+print countHistogram
+
+
+# for v in Values.itervalues():
+#     print v.allocation_id
