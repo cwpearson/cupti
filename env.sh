6c6d22403953c124223befbc53a475b585138e26
Carl Pearson <carl.w.pearson@gmail.com>
2017-11-21 08:40:45 -0600

    Read some vars from environment if set

--- env.sh
+++ env.sh
@@ -1,23 +1,41 @@
 #! /bin/bash
+set -eou pipefail
 
 ## Adjust these variables to match your installation
 # CUPTI should be in the LD_LIBRARY_PATH
+
+if [ -z "${OPENTRACING_LIB+xxx}" ]; then 
+  export OPENTRACING_LIB="$HOME/software/opentracing-cpp/lib";
+fi
+if [ -z "${ZIPKIN_LIB+xxx}" ]; then 
+  export ZIPKIN_LIB="$HOME/software/zipkin-cpp-opentracing/lib";
+fi
+
 export LD_LIBRARY_PATH="/usr/local/cuda/extras/CUPTI/lib64:$LD_LIBRARY_PATH"
-export LD_LIBRARY_PATH="$HOME/software/opentracing-cpp/lib:$LD_LIBRARY_PATH"
-export LD_LIBRARY_PATH="$HOME/software/zipkin-cpp-opentracing/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$OPENTRACING_LIB:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$ZIPKIN_LIB:$LD_LIBRARY_PATH"
+
 # where to look for prof.so
-export CPROF_ROOT="$HOME/repos/cupti"
-
-
+if [ -z "${CPROF_ROOT+xxx}" ]; then 
+  export CPROF_ROOT="$HOME/repos/cupti"; # not set at all
+fi
 
 ## Control some profiling parameters.
-# Check env.sh for more info
+
+# default output file
 export CPROF_OUT="output.cprof"
 
-
+# endpoint for tracing
+export CPROF_USE_ZIPKIN=1
+export CPROF_ZIPKIN_HOST=34.215.126.137
+export CPROF_ZIPKIN_PORT=16686
 
 ## Run the provided program. For example
 #   ./env.sh examples/samples/vectorAdd/vec
-LD_PRELOAD="$LD_PRELOAD:$CPROF_ROOT/lib/libcprof.so" $@
 
-# set environment LD_LIBRARY_PATH=/usr/local/cuda/extras/CUPTI/lib64:/home/pearson/software/opentracing-cpp/lib:/home/pearson/software/zipkin-cpp-opentracing/lib
+if [ -z "${LD_PRELOAD+xxx}" ]; then 
+echo $@;
+  LD_PRELOAD="$CPROF_ROOT/lib/libcprof.so" $@; # unset
+else
+  LD_PRELOAD="$LD_PRELOAD:$CPROF_ROOT/lib/libcprof.so" $@; # unset
+fi
