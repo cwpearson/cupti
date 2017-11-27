c266f7bbc8f5b21464abba2e9a4b52d21b3eb1ad
Carl Pearson <pearson@illinois.edu>
2017-11-27 13:21:45 -0600

    ld_preload ncclCommInit*

--- src/preload_nccl.cpp
+++ src/preload_nccl.cpp
@@ -11,42 +11,79 @@
 #include "cprof/profiler.hpp"
 #include "cprof/values.hpp"
 
-#define NCCL_DLSYM_BOILERPLATE(name)                                          \
+#define NCCL_DLSYM_BOILERPLATE(name)                                           \
   static name##Func real_##name = nullptr;                                     \
   cprof::err() << "LD_PRELOAD intercept: " #name << std::endl;                 \
   if (real_##name == nullptr) {                                                \
     {                                                                          \
-      void *h = dlopen("libnccl.so", RTLD_LAZY);                              \
+      void *h = dlopen("libnccl.so", RTLD_LAZY);                               \
       real_##name = (name##Func)dlsym(h, #name);                               \
     }                                                                          \
   }                                                                            \
   assert(real_##name && "Will the real " #name " please stand up?");
 
-typedef ncclResult_t (*ncclBcastFunc)(void* buff, int count, ncclDataType_t datatype, int root,
-ncclComm_t comm, cudaStream_t stream);
-extern "C" ncclResult_t ncclBcast(void* buff, int count, ncclDataType_t datatype, int root,
-ncclComm_t comm, cudaStream_t stream) {
-  NCCL_DLSYM_BOILERPLATE(ncclBcast);
-
+typedef ncclResult_t (*ncclCommInitAllFunc)(ncclComm_t *comms, int nGPUs,
+                                            const int *devList);
+extern "C" ncclResult_t ncclCommInitAll(ncclComm_t *comms, int nGPUs,
+                                        const int *devList) {
+  NCCL_DLSYM_BOILERPLATE(ncclCommInitAll);
   cprof::err() << "WARN: tid " << cprof::model::get_thread_id()
-               << " disabling CUPTI callbacks during ncclBcast"
+               << " disabling CUPTI callbacks during ncclBCommInitAll"
                << std::endl;
 
-  cprof::err() << "WARN: not doing anything with ncclBcast" << std::endl;
-
   cprof::driver().this_thread().pause_cupti_callbacks();
-
-  const ncclResult_t ret = real_ncclBcast(buff, count, datatype, root, comm, stream);
+  const ncclResult_t ret = real_ncclCommInitAll(comms, nGPUs, devList);
   cprof::driver().this_thread().resume_cupti_callbacks();
   return ret;
 }
 
+typedef ncclResult_t (*ncclCommInitRankFunc)(ncclComm_t *comm, int nGPUs,
+                                             ncclUniqueId cliqueId, int rank);
+extern "C" ncclResult_t ncclCommInitRank(ncclComm_t *comm, int nGPUs,
+                                         ncclUniqueId cliqueId, int rank) {
+  NCCL_DLSYM_BOILERPLATE(ncclCommInitRank);
+  cprof::err() << "WARN: tid " << cprof::model::get_thread_id()
+               << " disabling CUPTI callbacks during ncclCommInitRank"
+               << std::endl;
 
-typedef ncclResult_t (*ncclAllReduceFunc)(const void* sendbuff, void* recvbuff, int count,
-ncclDataType_t datatype, ncclRedOp_t op, ncclComm_t comm, cudaStream_t stream);
+  cprof::driver().this_thread().pause_cupti_callbacks();
+  const ncclResult_t ret = real_ncclCommInitRank(comm, nGPUs, cliqueId, rank);
+  cprof::driver().this_thread().resume_cupti_callbacks();
+  return ret;
+}
 
-extern "C" ncclResult_t  ncclAllReduce(const void* sendbuff, void* recvbuff, int count,
-ncclDataType_t datatype, ncclRedOp_t op, ncclComm_t comm, cudaStream_t stream) {
+typedef ncclResult_t (*ncclBcastFunc)(void *buff, int count,
+                                      ncclDataType_t datatype, int root,
+                                      ncclComm_t comm, cudaStream_t stream);
+extern "C" ncclResult_t ncclBcast(void *buff, int count,
+                                  ncclDataType_t datatype, int root,
+                                  ncclComm_t comm, cudaStream_t stream) {
+  NCCL_DLSYM_BOILERPLATE(ncclBcast);
+
+  cprof::err() << "WARN: tid " << cprof::model::get_thread_id()
+               << " disabling CUPTI callbacks during ncclBcast" << std::endl;
+
+  // current_device buf deps on root buff
+  // const int devId = cprof::driver().current_device();
+
+  // auto rootBufVal = Values::instance().find();
+
+  cprof::driver().this_thread().pause_cupti_callbacks();
+  const ncclResult_t ret =
+      real_ncclBcast(buff, count, datatype, root, comm, stream);
+  cprof::driver().this_thread().resume_cupti_callbacks();
+  return ret;
+}
+
+typedef ncclResult_t (*ncclAllReduceFunc)(const void *sendbuff, void *recvbuff,
+                                          int count, ncclDataType_t datatype,
+                                          ncclRedOp_t op, ncclComm_t comm,
+                                          cudaStream_t stream);
+
+extern "C" ncclResult_t ncclAllReduce(const void *sendbuff, void *recvbuff,
+                                      int count, ncclDataType_t datatype,
+                                      ncclRedOp_t op, ncclComm_t comm,
+                                      cudaStream_t stream) {
   NCCL_DLSYM_BOILERPLATE(ncclAllReduce);
 
   cprof::err() << "WARN: tid " << cprof::model::get_thread_id()
@@ -57,8 +94,8 @@ ncclDataType_t datatype, ncclRedOp_t op, ncclComm_t comm, cudaStream_t stream) {
 
   cprof::driver().this_thread().pause_cupti_callbacks();
 
-  const ncclResult_t ret = real_ncclAllReduce(sendbuff, recvbuff, count, datatype, op, comm, stream);
+  const ncclResult_t ret =
+      real_ncclAllReduce(sendbuff, recvbuff, count, datatype, op, comm, stream);
   cprof::driver().this_thread().resume_cupti_callbacks();
   return ret;
 }
-
