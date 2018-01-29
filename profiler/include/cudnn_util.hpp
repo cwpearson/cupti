9a8652493ab30c9eb88c2fbc7cff2bec29d379f4
Carl Pearson <carl.w.pearson@gmail.com>
2018-01-29 09:47:27 -0600

    add forgotten files

---
+++ profiler/include/cudnn_util.hpp
@@ -0,0 +1,23 @@
+#ifndef UTIL_CUDNN_HPP
+#define UTIL_CUDNN_HPP
+
+#include <cstdlib>
+#include <cudnn.h>
+#include <ostream>
+
+#define CUDNN_CHECK(ans, err)                                                  \
+  { cudnnAssert((ans), __FILE__, __LINE__, (err)); }
+inline void cudnnAssert(cudnnStatus_t code, const char *file, int line,
+                        std::ostream &err, bool abort = true) {
+  if (code != CUDNN_STATUS_SUCCESS) {
+    const char *errstr = cudnnGetErrorString(code);
+    err << "CUDNN_CHECK: " << errstr << " " << file << " " << line << std::endl;
+    if (abort)
+      exit(code);
+  }
+}
+
+size_t cudnnDataTypeSize(const cudnnDataType_t type);
+size_t cudnnTensor4dSize(const cudnnTensorDescriptor_t tensorDesc);
+
+#endif
