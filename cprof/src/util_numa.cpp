5975632b0b73306023fc2fe81388b77ac1f15f23
Carl Pearson <carl.w.pearson@gmail.com>
2018-02-05 06:32:15 -0600

    add forgotten util_numa files

---
+++ cprof/src/util_numa.cpp
@@ -0,0 +1,26 @@
+#include "numaif.h"
+#include "unistd.h"
+#include <cassert>
+
+#include "cprof/util_numa.hpp"
+
+static void *get_page(const void *ptr, const int page_size) {
+  const auto u = reinterpret_cast<uintptr_t>(ptr);
+  const auto r = (u / page_size) * page_size;
+  return reinterpret_cast<void *>(r);
+}
+
+int get_numa_node(const void *ptr) {
+  const int page_size = getpagesize();
+  void *start_of_page = get_page(ptr, page_size);
+  int status = -1;
+  long ret_code;
+  ret_code =
+      move_pages(0 /*self memory */, 1, &start_of_page, NULL, &status, 0);
+  assert(!ret_code && "Handle this error.");
+  return status;
+}
+
+int get_numa_node(const uintptr_t ptr) {
+  return get_numa_node(reinterpret_cast<const void *>(ptr));
+}
