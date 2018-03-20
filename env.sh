04f225797aeaa3f8d1d6434d807395aaf47d8aa4
Carl Pearson <pearson@illinois.edu>
2018-03-20 12:42:17 -0500

    default CPROF to not quiet

--- env.sh
+++ env.sh
@@ -38,6 +38,7 @@ export CPROF_ZIPKIN_PORT=9411
 export CPROF_CHROME_TRACING=events_"$NOW".json
 export CPROF_CUPTI_DEVICE_BUFFER_SIZE=1024
 export CPROF_MODE=full
+export CPROF_QUIET=0
 
 ## Run the provided program. For example
 #   ./env.sh examples/samples/vectorAdd/vec
