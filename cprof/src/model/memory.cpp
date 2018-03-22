a218b575ef05e1787a4e893e012f22aeed4b9fa7
Carl Pearson <carl.w.pearson@gmail.com>
2018-03-22 06:40:29 -0500

    break switch cases in cprof/memory.cpp

--- cprof/src/model/memory.cpp
+++ cprof/src/model/memory.cpp
@@ -7,14 +7,19 @@ json cprof::model::to_json(const Memory &m) {
   switch (m) {
   case Memory::Unknown:
     j["type"] = "unknown";
+    break;
   case Memory::Pageable:
     j["type"] = "pageable";
+    break;
   case Memory::Pagelocked:
     j["type"] = "pagelocked";
+    break;
   case Memory::Unified3:
     j["type"] = "unified3";
+    break;
   case Memory::Unified6:
     j["type"] = "unified6";
+    break;
   default:
     assert(0 && "Unexpected memory type");
   }
