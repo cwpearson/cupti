7479a006e0133aa15e5108dde5da713a943d6669
Carl Pearson <pearson@illinois.edu>
2018-03-22 12:44:05 -0500

    add output handler for CUpti_Activity

---
+++ profiler/include/cupti_activity_out.hpp
@@ -0,0 +1,8 @@
+#ifndef CUPTI_ACTIVITY_OUT_HPP
+#define CUPTI_ACTIVITY_OUT_HPP
+
+#include "cupti_activity.hpp"
+
+void out_activityHander(const CUpti_Activity *record);
+
+#endif
---
+++ profiler/src/cupti_activity_out.cpp
@@ -0,0 +1,46 @@
+#include <string>
+
+#include "cprof/activity/compute.hpp"
+#include "cprof/activity/transfer.hpp"
+
+#include "cupti_activity_out.hpp"
+#include "profiler.hpp"
+
+//
+// KERNEL
+//
+static void handleKernel(const CUpti_ActivityKernel3 *record) {
+  assert(record);
+  auto compute = cprof::activity::Compute(record);
+  profiler::atomic_out(compute.json());
+}
+
+//
+// MEMCPY
+//
+static void handleMemcpy(const CUpti_ActivityMemcpy *record) {
+  assert(record);
+  auto transfer = cprof::activity::Transfer(record);
+  profiler::atomic_out(transfer.json());
+}
+
+void out_activityHander(const CUpti_Activity *record) {
+
+  switch (record->kind) {
+  case CUPTI_ACTIVITY_KIND_KERNEL: {
+    auto activity_cast =
+        reinterpret_cast<const CUpti_ActivityKernel3 *>(record);
+    handleKernel(activity_cast);
+    break;
+  }
+  case CUPTI_ACTIVITY_KIND_MEMCPY: {
+    auto activity_cast = reinterpret_cast<const CUpti_ActivityMemcpy *>(record);
+    handleMemcpy(activity_cast);
+    break;
+  }
+  default: {
+    //   auto activity_cast = record;
+    break;
+  }
+  };
+}
