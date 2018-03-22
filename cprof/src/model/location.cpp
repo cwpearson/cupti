fc91ee605d1ea9bdb5306fd9ddaaa30dbfb92ba9
Carl Pearson <carl.w.pearson@gmail.com>
2018-03-22 06:43:41 -0500

    have location.to_json() return json, not a string

--- cprof/src/model/location.cpp
+++ cprof/src/model/location.cpp
@@ -7,7 +7,7 @@ json Location::to_json() const {
   json j;
   j["type"] = to_string(type_);
   j["id"] = id_;
-  return j.dump();
+  return j;
 }
 
 std::string Location::to_json_string() const { return to_json().dump(); }
